fa79ece34346ca011e1f3bfbc946216beca5147c
Dafreak99 <sl4mdunk1999@gmail.com>
2024-11-13 20:57:00 +0700

    feat: integrate Prisma to Catalog service

--- catalog_service/src/repository/catalog.repository.ts
+++ catalog_service/src/repository/catalog.repository.ts
@@ -1,26 +1,45 @@
+import { PrismaClient } from '@prisma/client';
 import { ICatalogRepository } from '../interfaces/catalogRepository.inteface';
 import { Product } from '../models/product.model';
-import { ProductFactory } from '../utils/fixtures';
 
 export class CatalogRepository implements ICatalogRepository {
+  prisma: PrismaClient;
+
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+
   async create(data: Product): Promise<Product> {
-    const product = ProductFactory.build();
-    return Promise.resolve(product);
+    return await this.prisma.product.create({
+      data,
+    });
   }
   async update(data: Product): Promise<Product> {
-    const product = ProductFactory.build();
-    return Promise.resolve(product);
+    return await this.prisma.product.update({
+      where: { id: data.id },
+      data,
+    });
   }
-  async delete(id: number): Promise<number> {
-    const product = ProductFactory.build();
-    return Promise.resolve(Number(product.id));
+  async delete(id: number) {
+    return await this.prisma.product.delete({
+      where: { id },
+    });
   }
   async find(limit: number, offset: number): Promise<Product[]> {
-    const products = ProductFactory.buildList(limit);
-    return Promise.resolve(products);
+    return await this.prisma.product.findMany({
+      take: limit,
+      skip: offset,
+    });
   }
   async findOne(id: number): Promise<Product> {
-    const product = ProductFactory.build();
-    return Promise.resolve(product);
+    const product = await this.prisma.product.findFirst({
+      where: { id },
+    });
+
+    if (!product) {
+      throw new Error('Product not found');
+    }
+
+    return product;
   }
 }
